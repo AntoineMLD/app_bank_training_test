eb37de320b969d394a85779b6c1eb35d64ca19fc
AntoineMLD <antoine.moulard@hotmail.com>
2024-05-14 11:29:11 +0200

    start classes (bank.py) and init_db files

---
+++ bank.py
@@ -0,0 +1,33 @@
+class Account:
+
+    def __init__(self, id_account, initial_deposit):
+        self.id_account = id_account
+        self.initial_deposit = initial_deposit
+
+    def create_account(self):
+        pass
+
+
+    def get_balance(self):
+        pass
+
+
+
+class Transation:
+
+    def __init__(self, amount_to, amount_from):
+        self.amount_to = amount_to
+        self.amount_from = amount_from
+
+    def deposit(self):
+        pass
+
+
+    def withdraw(self):
+        pass
+
+
+    def transfer(self):
+        pass
+
+
